c962b30ffc30a66f407f45010f5695eedb920f05
habioye <78454514+habioye@users.noreply.github.com>
2024-05-21 00:23:47 -0500

    Added Directions as well as formatting

--- Week 1/Day 1/Classic_Mode.java
+++ Week 1/Day 1/Classic_Mode.java
@@ -2,18 +2,15 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Classic_Mode {
-    // private int current;
-    // private int attempts;
-    // public Advanced_Number_Guessing_Game() {
-    //     // Random rand = new Random();
 
-    //     // this.current = 1 + rand.nextInt(100);
-    //     // this.attempts = 0;
-
-
-
-    // }
     public static void main(String[] args) {
+        System.out.println("Advanced Number Guessing Game");
+        System.out.println("---------------------------------");
+        System.out.println("Directions");
+        System.err.println("The program generates a random number between 1 and 100.\n" + //
+                        "The user has to guess the number, and the program provides hints (higher or lower) until the user guesses correctly.\n" + //
+                        "The number of attempts will be displayed at the end.");
+        System.out.println("---------------------------------");
         Scanner scanner = new Scanner(System.in);
         Random rand = new Random();
 
