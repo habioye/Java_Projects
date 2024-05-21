dbff8a9054518af65dc0ae86f4816252ee2e138b
habioye <78454514+habioye@users.noreply.github.com>
2024-05-21 01:12:47 -0500

    Refactored and fixed grammatical mistakes. In addition I added some comments.

--- Week 1/Day 1/Difficulty_Levels.java
+++ Week 1/Day 1/Difficulty_Levels.java
@@ -5,63 +5,63 @@ public class Difficulty_Levels {
 
     public static void main(String[] args) {
         System.out.println("Advanced Number Guessing Game");
-        System.out.println("------------------------");
+        System.out.println("---------------------------------");
+        System.out.println("Directions:");
+        System.err.println(
+                """
+                        The program generates a random number between 1 and 100.
+                        The user has to guess the number, and the program provides hints (higher or lower) until the user guesses correctly.
+                        The number of attempts will be displayed at the end.""");
+        System.out.println("---------------------------------");
         System.out.println("Choose Your Difficulty Level");
         System.out.println("1.) Easy : Number between 1 and 50. 30 Attempts.");
         System.out.println("2.) Medium : Number between 1 and 100. 25 Attempts.");
         System.out.println("3.) Hard : Number between 1 and 500. 20 Attempts.");
-        
-        
-        
+
         Scanner scanner = new Scanner(System.in);
         Random rand = new Random();
         int difficulty = scanner.nextInt();
         int bound = 0;
         int attemptlimit = 0;
         switch (difficulty) {
-            case 1:
+            case 1 -> {
                 bound = 50;
                 attemptlimit = 30;
-                break;
-            case 2:
+            }
+            case 2 -> {
                 bound = 100;
                 attemptlimit = 25;
-                break;
-            case 3:
+            }
+            case 3 -> {
                 bound = 500;
                 attemptlimit = 20;
-                break;
-            default:
-                break;
+            }
+            default -> throw new IllegalArgumentException("No valid input detected");
         }
-        int current = 1+rand.nextInt(bound);
+        int target = 1 + rand.nextInt(bound);
         int attempts = 0;
-        
-        
-
-
 
         while (true) {
             System.out.println("Guess The number: ");
             int number = scanner.nextInt();
-            attempts+=1;
+            attempts += 1;
 
-            if (number < current) {
-                System.out.println("Higher");
-            } else if (number > current) {
-                System.out.println("Lower");
+            if (number < target) {
+                System.out.println("The answer is higher");
+
+            } else if (number > target) {
+                System.out.println("The answer is lower");
             } else {
-                System.out.println("Correct!!");
+                System.out.println("Correct!");
                 break;
             }
-            attempts+=1;
+
             if (attempts >= attemptlimit) {
-                System.out.println("Above the number of Attempts limit");
+                System.out.println("Ran out of attempts!");
                 break;
             }
         }
-        System.out.println("The number of Attempts is "+ attempts);
-        return;
+        System.out.println("The number of attempts is " + attempts);
 
     }
 
