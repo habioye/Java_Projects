02507185cfd1a7d545347cb80eb45c2dd82dd9dd
habioye <78454514+habioye@users.noreply.github.com>
2024-05-21 00:36:44 -0500

    Added comments and refactored code

--- Week 1/Day 1/Classic_Mode.java
+++ Week 1/Day 1/Classic_Mode.java
@@ -3,39 +3,46 @@ import java.util.Scanner;
 
 public class Classic_Mode {
 
+    // The program will generate a random number and the user has to guess the
+    // answer(with hints).
+    // When the correct answer is given the number of attempts is displayed..
     public static void main(String[] args) {
         System.out.println("Advanced Number Guessing Game");
         System.out.println("---------------------------------");
-        System.out.println("Directions");
-        System.err.println("The program generates a random number between 1 and 100.\n" + //
-                        "The user has to guess the number, and the program provides hints (higher or lower) until the user guesses correctly.\n" + //
-                        "The number of attempts will be displayed at the end.");
+        System.out.println("Directions:");
+        System.err.println(
+            """
+                The program generates a random number between 1 and 100.
+                The user has to guess the number, and the program provides hints (higher or lower) until the user guesses correctly.
+                The number of attempts will be displayed at the end.
+            """ 
+        );
         System.out.println("---------------------------------");
-        Scanner scanner = new Scanner(System.in);
-        Random rand = new Random();
-
-        int current = 1+rand.nextInt(100);
-        int attempts = 0;
-
-
-        while (true) {
-            System.out.println("Guess The number: ");
-            int number = scanner.nextInt();
-            attempts+=1;
-            if (number < current) {
-                System.out.println("Higher");
-            } else if (number > current) {
-                System.out.println("Lower");
-            } else {
-                System.out.println("Correct!!");
-                System.out.println("The number of Attempts is "+ attempts);
-                break;
-            }
-
+        try (Scanner scanner = new Scanner(System.in)) {
+            Random rand = new Random();
             
-
+            int target = 1 + rand.nextInt(100);
+            int attempts = 0;
+            
+            // Loop to keep checking if the given number matches the target.
+            while (true) {
+                System.out.println("Guess The number: ");
+                int number = scanner.nextInt();
+                attempts += 1;
+                if (number < target) {
+                    System.out.println("The answer is higher");
+                } else if (number > target) {
+                    System.out.println("The answer is lower");
+                } else {
+                    System.out.println("Correct!!");
+                    System.out.println("The number of Attempts is " + attempts);
+                    break;
+                }
+                
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
-        return;
 
     }
 }
