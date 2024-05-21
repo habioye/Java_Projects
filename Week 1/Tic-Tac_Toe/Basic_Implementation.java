e92f87247a686d0c49d9c64346b9147a076574b2
habioye <78454514+habioye@users.noreply.github.com>
2024-05-21 10:27:48 -0500

    Fixed the display board

--- Week 1/Tic-Tac_Toe/Basic_Implementation.java
+++ Week 1/Tic-Tac_Toe/Basic_Implementation.java
@@ -1,27 +1,36 @@
 
+import java.util.Arrays;
+
 public class Basic_Implementation {
-    private void DisplayBoard(String[][] board) {
+    private static void DisplayBoard(String[][] board) {
         int length = board.length;
         for (int i = 0; i < length; i++) {
             for (int j = 0; j < length; j++) {
                 System.out.print(board[i][j] + " |");
             }
-            for (int j = 0; j < length; j++) {
-                System.out.print("__");
-            }
+            System.out.print("\n");
+            // for (int j = 0; j < length; j++) {
+            System.out.println("___".repeat(board.length));
+            // }
         }
 
     }
 
-    private boolean DetectWin(String[][] board) {
+    private static boolean DetectWin(String[][] board) {
+        String empty = " ";
+        String start;
+        int same;
         // Horizontal Win
         for (int i = 0; i < board.length; i++) {
-            String start = board[i][0];
-            int same = 0;
+            start = board[i][0];
+            if (start == empty)
+                continue;
+
+            same = 0;
             for (int j = 0; j < board.length; j++) {
-                if (board[i][j] != start) {
+                if (board[i][j] != start)
                     break;
-                }
+
                 same += 1;
             }
             if (same == board.length) {
@@ -31,24 +40,79 @@ public class Basic_Implementation {
         }
 
         // Vertical Win
-        for (int i = 0; i < 10; i++) {
-            String start = board[i][0];
-            int same = 0;
+        for (int j = 0; j < 10; j++) {
+            start = board[0][j];
+            if (start == empty)
+                continue;
+            same = 0;
+            for (int i = 0; i < board.length; i++) {
+                if (board[i][j] != start) {
+                    break;
+
+                }
+                same += 1;
+            }
+            if (same == board.length)
+                return true;
 
         }
 
-    }
+        // Diagonal Win
+        start = board[0][0];
+        if (start != empty) {
+            same = 0;
+            for (int i = 0; i < board.length; i++) {
+                if (board[i][i] != start) {
+                    break;
+                }
+                same += 1;
+            }
+            if (same == board.length) {
+                return true;
+            }
+        }
+
+        start = board[board.length - 1][0];
+        same = 0;
+        for (int i = 0; i < board.length; i++) {
+            if (board[board.length - 1 - i][i] != start) {
+                break;
+            }
+            same += 1;
+        }
+
+        if (same == board.length) {
+            return true;
+        }
+
+        return false;
 
     }
 
     public static void main(String[] args) {
         String[][] board = new String[3][3];
+        for (String[] row : board) {
+            Arrays.fill(row, " ");
+        }
         boolean first = true;
         int length = board.length;
 
-        while (true) {
+        System.out.println("Tic-Tac-Toe");
+        System.out.println("------------------------------");
+        DisplayBoard(board);
 
-        }
+        int positions = length * length;
+
+        // while (true) {
+        // System.out.println("Select a position");
+        // if (first) {
+        // System.out.println("It is currently player1's turn");
+        // } else {
+
+        // System.out.println("It is currently player2's turn");
+        // }
+
+        // }
 
     }
 }
