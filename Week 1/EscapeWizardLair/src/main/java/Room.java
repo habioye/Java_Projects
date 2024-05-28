c9774431d3adbc5938a9d1739e8d7b9cd8bfe859
habioye <78454514+habioye@users.noreply.github.com>
2024-05-28 08:18:45 -0400

    Reformatting the room display

--- Week 1/EscapeWizardLair/src/main/java/Room.java
+++ Week 1/EscapeWizardLair/src/main/java/Room.java
@@ -10,7 +10,7 @@ public class Room {
     // encapsulation for room name
     public String extractName (Room room) {
         if (room == null) return "Dirt";
-        return this.roomName;
+        return room.roomName;
     }
 
 //    Displays the rooms that surround and gives them the names for the objects in the room
@@ -25,19 +25,43 @@ public class Room {
         room4 = extractName(connections[3]);
 
         System.out.println("Room Name: "+this.roomName);
+        System.out.println(room3);
 //        if (connections[0] == None)
         // Top, Left, Right, Down
-        System.out.printf(String.format("""
-                ________________| %s |______________
-                                |                                |
-                 %s    |        %s       |         %s
-               _________________|________________________________|_____________
-                                | %s |
-                """,extractName(connections[0]),extractName(connections[1]),this.roomName,extractName(connections[2]),extractName(connections[3]));
+        int cellwidth = 10;
+        StringBuilder sb = new StringBuilder();
 
+        String border = String.format("""
+                +%s+%s+%s+
+                ""","-".repeat(cellwidth),"-".repeat(cellwidth),"-".repeat(cellwidth));
+
+        // Function to generate a row with names
+            sb.append(border);
+            sb.append(String.format("""
+                    |%-10s|%-10s|%-10s|
+                    ""","",room1,""));
+            sb.append(border);
+            sb.append(String.format("""
+                    |%-10s|%-10s|%-10s|
+                    """,room2,this.roomName,room3));
+            sb.append(border);
+            sb.append(String.format("""
+                    |%-10s|%-10s|%-10s|
+                    ""","",room4,""));
+            sb.append(border);
+            System.out.println(sb.toString());
+//        System.out.printf(String.format("""
+//                ________________| %s |______________
+//                                |                                |
+//                 %s    |        %s       |         %s
+//               _________________|________________________________|_____________
+//                                | %s |
+//                """,room1,room2,this.roomName,room3,room4);
+
+        Item item = new Item();
 
         for (int i = 0; i < this.objects.size();i++) {
-            Item item = new Item();
+            item = this.objects.get(i);
             System.out.println((i+1)+".)"+item.getName());
 
         }
